f0d188911c75817c1ecfc75a432756b889b3b395
Sam Laister <saml@everbit.dev>
2024-09-22 20:51:58 +0100

    Day 1 and 2 - no comments

--- solutions/day01.dart
+++ solutions/day01.dart
@@ -1,39 +1,102 @@
 import '../utils/index.dart';
 
-/// Every day should extend [GenericDay] to have access to the corresponding
-/// input and a common interface.
-///
-/// Naming convention is set to pad any single-digit day with `0` to have proper
-/// ordering of files and correct mapping between input for days and the day
-/// files.
 class Day01 extends GenericDay {
-  // call the superclass with an integer == today´s day
   Day01() : super(1);
 
-  /// The [InputUtil] can be accessed through the superclass variable `input`. \
-  /// There are several methods in that class that parse the input in different
-  /// ways, an example is given below
-  ///
-  /// The return type of this is `dynamic` for [GenericDay], so you can decide
-  /// on a day-to-day basis what this function should return.
   @override
   List<int> parseInput() {
     final lines = input.getPerLine();
-    // exemplary usage of ParseUtil class
     return ParseUtil.stringListToIntList(lines);
   }
 
-  /// The `solvePartX` methods always return a int, the puzzle solution. This
-  /// solution will be printed in main.
   @override
   int solvePart1() {
-    // TODO implement
-    return 0;
+    final values = input.getBy('\n').map((e) {
+      if (e.isEmpty) return 0;
+      int f1 = 0;
+      int f2 = e.length - 1;
+
+      int? value1;
+      int? value2;
+
+      while (value1 == null || value2 == null) {
+        if (value1 == null) {
+          value1 = int.tryParse(e[f1]);
+          f1++;
+        }
+
+        if (value2 == null) {
+          value2 = int.tryParse(e[f2]);
+          f2--;
+        }
+      }
+
+      return int.parse('$value1$value2');
+    }).toList();
+
+    return values.fold(0, (a, b) => a + b);
   }
 
   @override
   int solvePart2() {
-    // TODO implement
-    return 0;
+    const List<String> words = [
+      'one',
+      'two',
+      'three',
+      'four',
+      'five',
+      'six',
+      'seven',
+      'eight',
+      'nine',
+    ];
+    final List<int> values = input.getBy('\n').map((e) {
+      if (e.isEmpty) return 0;
+      int f1 = 0;
+      int f2 = e.length - 1;
+
+      int? value1;
+      int? value2;
+
+      while (value1 == null || value2 == null) {
+        // Check word first
+
+        if (value1 == null) {
+          for (int i = 0; i < words.length; i++) {
+            if (e.substring(f1).startsWith(words[i])) {
+              value1 = i + 1;
+              f1 += words[i].length;
+              break;
+            }
+          }
+        }
+
+        if (value2 == null) {
+          for (int i = 0; i < words.length; i++) {
+            if (e.substring(0, f2 + 1).endsWith(words[i])) {
+              value2 = i + 1;
+              f2 -= words[i].length;
+              break;
+            }
+          }
+        }
+
+        // Check digits
+
+        if (value1 == null) {
+          value1 = int.tryParse(e[f1]);
+          f1++;
+        }
+
+        if (value2 == null) {
+          value2 = int.tryParse(e[f2]);
+          f2--;
+        }
+      }
+
+      return int.parse('$value1$value2');
+    }).toList();
+
+    return values.fold(0, (a, b) => a + b);
   }
 }
